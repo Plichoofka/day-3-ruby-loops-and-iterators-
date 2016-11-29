1854a17bd0beb56e75119ef42614860dcd0dc2d5
Anna Plich <aplich86@gmail.com>
2016-11-29 16:57:41 +0100

    Print out the Ingredients

--- spanish_omlette.rb
+++ spanish_omlette.rb
@@ -7,3 +7,25 @@ def print_progress_bar
   3.times { sleep 0.5; print "." }
   puts "\n"
 end
+
+ # 1. defining ingredients
+NUM_POTATOES = 5 #constant - won't change the value
+NUM_EGGS = 6 #constant - won't change the value
+
+ingredients = [
+  { name: 'potatoes', quantity: NUM_POTATOES },
+  { name: 'eggs', quantity: NUM_EGGS },
+  { name: 'onion', quantity: 1 },
+  { name: 'oil', quantity: 'some' },
+  { name: 'salt', quantity: 'some' },
+]
+
+# Print out the ingredients
+puts "*    Let's cook a Spanish Omelette!    *"
+print_divider
+
+puts "You need the following ingredients:"
+
+ingredients.each do |ingredient|
+  puts "* #{ingredient[:quantity]} #{ingredient[:name]}"
+end
